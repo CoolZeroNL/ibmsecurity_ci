c2d094c2787d79a7fde441f0315af78073e3f679
CoolZeroNL <info@local-events.nl>
2020-03-02 14:12:00 +0100

    Auto Add: 02-03-2020 14:12:00 [skip ci]

--- awx-ansible-playbook-sample/tmp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-        # install for AWX Tower.
-        if [ -d ~/.ansible ] 
-        then
-           echo "Installing into: ~/.ansible/collections/"
-           cp -R awx_collection ~/.ansible/collections/
-        fi
