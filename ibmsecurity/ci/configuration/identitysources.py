b657e0ed2bbf42f85d2ca28f62d92afe395675c1
CoolZeroNL <info@local-events.nl>
2020-03-24 17:39:04 +0100

    Auto Add: 24-03-2020 17:39:03 [skip ci]

--- ibmsecurity/ci/configuration/identitysources.py
+++ ibmsecurity/ci/configuration/identitysources.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 def get_all(ciAppliance, check_mode=False, force=False):
     """
     Gets the list of all Identity Sources.
-    """
+    """ 
     return ciAppliance.invoke_get("Retrieving Identity Sources", "/v1.0/identitysources")
 
 
@@ -18,20 +18,37 @@ def get_all(ciAppliance, check_mode=False, force=False):
     # Get ID of `idenity source`
         # Check each application if ID in "identitySources": [], is found.
               
+def resolve_name_to_id(ciAppliance, name):
+    """
+    resolve
+    """ 
+    name = name.lower()
+    data = get_all(ciAppliance=ciAppliance)
+
+    for node in data['data']['identitySources']:
+        if node['instanceName'].lower() == name:
+            id = node['id']
+            if id != '':    
+                return id
+            else:
+                return None
+                  
               
-              
-def add(ciAppliance, sourcetypeid, instancename, enabled, properties_client_id_sensitive, properties_client_id_value, properties_client_secret_sensitive, properties_client_secret_value, properties_identitylinkingenabled_sensitive, properties_identitylinkingenabled_value, properties_identitylinkingjitenabled_sensitive, properties_identitylinkingjitenabled_value, properties_identitylinkingprincipalattribute_sensitive, properties_identitylinkingprincipalattribute_value, properties_realm_sensitive, properties_realm_value, check_mode=False, force=False):
+def add(ciAppliance, sourcetypeid, instancename, new_instancename, enabled, properties_client_id_sensitive, properties_client_id_value, properties_client_secret_sensitive, properties_client_secret_value, properties_identitylinkingenabled_sensitive, properties_identitylinkingenabled_value, properties_identitylinkingjitenabled_sensitive, properties_identitylinkingjitenabled_value, properties_identitylinkingprincipalattribute_sensitive, properties_identitylinkingprincipalattribute_value, properties_realm_sensitive, properties_realm_value, check_mode=False, force=False):
     """
     Add a Identity Source.
     """
     # todo:
     # - add nice check
     # - add 
+    
+
+
 
    # Create base Json
     client_json = {
         "sourceTypeId": sourcetypeid,
-        "instanceName": instancename,
+        "instanceName": new_instancename if new_instancename else instancename,
         "enabled": enabled,
         "properties": [{
             "sensitive": properties_realm_sensitive,
@@ -65,6 +82,11 @@ def add(ciAppliance, sourcetypeid, instancename, enabled, properties_client_id_s
     # if customicon != '':
     #     client_json['customIcon'] = customicon
     
-    return ciAppliance.invoke_post("add identity source", "/v1.0/identitysources", client_json)
+    id = resolve_name_to_id(ciAppliance, instancename)
+    if id is None:
+        return ciAppliance.invoke_post("POST Identity source", "/v1.0/identitysources", client_json)
+    else:
+        return ciAppliance.invoke_put("PUT Identity Sources", "/v1.0/identitysources/" + id, client_json)    
+            
+    return ciAppliance.create_return_object()
     
-    return ciAppliance.create_return_object()        
